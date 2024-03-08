594657cb3d8a433d97e9d6cc97e7e3619d745502
Isaak <isaac@thesheridans.email>
2024-03-08 00:06:42 -0500

    commit

---
+++ 2024/crescendo_config.js
@@ -0,0 +1,197 @@
+
+
+var config_data = `
+{
+  "dataFormat": "kvs",
+  "title": "Scouting PASS 2024",
+  "page_title": "Crescendo",
+  "checkboxAs": "10",
+  "prematch": [
+    { "name": "Scouter Initials",
+      "code": "s",
+      "type": "scouter",
+      "size": 5,
+      "maxSize": 5,
+      "required": "true"
+    },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2024scan",
+      "required": "true"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
+      },
+      "defaultValue": "qm",
+      "required": "true"
+    },
+    { "name": "Match #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 150,
+      "required": "true"
+    },
+    { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
+        "r3": "Red-3",
+        "b3": "Blue-3"
+      },
+      "required":"false"
+    },
+    { "name": "Team #",
+      "code": "t",
+      "type": "team",
+      "min": 1,
+      "max": 99999
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png",
+      "clickRestriction": "one",
+      "allowableResponses": "1 12 13 24 25 36 37 48 49 60 61 72",
+      "shape": "circle 5 black red true"
+    }
+  ],
+  "auton": [
+    { "name": "Leave Starting Zone",
+      "code": "al",
+      "type": "bool"
+    },
+    { "name": "Amp Scores",
+      "code": "aas",
+      "type": "counter"
+    },
+    { "name": "Speaker Scores",
+      "code": "ass",
+      "type": "counter"
+    }
+  ],
+  "teleop": [
+    { "name": "Amp Scores",
+      "code": "tas",
+      "type": "counter"
+    },
+    { "name": "Non-amped speaker",
+      "code": "tna",
+      "type": "counter"
+    },
+    { "name": "Amped speaker",
+      "code": "ta",
+      "type": "counter"
+    },
+    { "name": "Times Amplified",
+      "code": "tta",
+      "type": "counter"
+    },
+    { "name": "Pickup From",
+      "code": "tpu",
+      "type": "radio",
+      "choices": {
+        "s": "Source<br>",
+        "f": "Floor<br>",
+        "b": "Both<br>",
+        "x": "Not Attempted"
+      },
+      "defaultValue": "x"
+    }
+  ],
+  "endgame": [
+    
+
+
+  { "name": "Final Status",
+      "code": "fs",
+      "type":"radio",
+      "choices": {
+        "1": "Parked<br>",
+        "3": "Onstage<br>",
+        "4": "Onstage (Spotlit)<br>",
+        "a": "Attempted but failed<br>",
+        "x": "Not attempted"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Harmonie's scored",
+      "code": "hr",
+      "type": "counter"
+    },
+    { "name": "Note in Trap",
+      "code": "nit",
+      "type": "bool"
+    }
+  ],
+  "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
+      "type": "radio",
+      "choices": {
+        "1": "Very Effective<br>",
+        "0.5": "Average<br>",
+        "0": "Not Effective<br>",
+        "x": "Not Observed"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Defense Rating",
+      "code": "dr",
+      "type": "radio",
+      "choices": {
+        "1": "Excellent<br>",
+        "0.75": "Good<br>",
+        "0.5": "Average<br>",
+        "0.25": "Below Average<br>",
+        "x": "Did not play defense"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Speed Rating",
+      "code": "sr",
+      "type": "radio",
+      "choices": {
+        "1": "1 (slow)<br>",
+        "2": "2<br>",
+        "3": "3<br>",
+        "4": "4<br>",
+        "5": "5 (fast)"
+      },
+      "defaultValue":"3"
+    },
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Dropped Notes (>2)",
+      "code": "dn",
+      "type": "bool"
+    },
+    { "name": "Make good<br>alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 55
+    }
+  ]
+}`;
